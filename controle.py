e7da4c612f77bea560594e7d3a591f4f35017197
Lucas Ribeiro <lucasrarueira017@gmail.com>
2024-06-05 18:34:11 -0300

    PDV

--- controle.py
+++ controle.py
@@ -1,5 +1,8 @@
 from PyQt5 import uic,QtWidgets
 import mysql.connector
+
+
+
 banco = mysql.connector.connect(
     host = "localhost",
     user = "root",
@@ -46,6 +49,8 @@ def tela_cadastro():
     formulario_cadastro.lineEdit_4.setText("")
     formulario_cadastro.lineEdit_5.setText("")
     formulario_cadastro.radioButton.setCheckable(False)
+def formulario_voltar():
+    formulario_cadastro.close()
 
 def tela_lista():
     lista_produtos.show()
@@ -61,18 +66,64 @@ def tela_lista():
         for j in range(0,7):
             lista_produtos.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
 
+def excluir_dados():
+    linha = lista_produtos.tableWidget.currentRow()
+    lista_produtos.tableWidget.removeRow(linha)
 
-  
+    cursor = banco.cursor()
+    cursor.execute("SELECT id FROM produtos")
+    dados_lidos = cursor.fetchall()
+    valor_id = dados_lidos[linha][0]
+    cursor.execute("DELETE FROM produtos WHERE id="+ str(valor_id))
 
+def lista_voltar():
+    lista_produtos.close()
 
+def editar_voltar():
+    editar_produto.close()
+
+def tela_editar():
+    linha = lista_produtos.tableWidget.currentRow()
+
+    cursor = banco.cursor()
+    cursor.execute("SELECT id FROM produtos")
+    dados_lidos = cursor.fetchall()
+    valor_id = dados_lidos[linha][0]
+    cursor.execute("SELECT * FROM produtos WHERE id="+ str(valor_id))
+    produto = cursor.fetchall()
+
+    editar_produto.show()
+
+    editar_produto.lineEdit_7.setText(str(produto[0][0]))
+    editar_produto.lineEdit.setText(str(produto[0][1]))
+    editar_produto.lineEdit_2.setText(str(produto[0][2]))
+    editar_produto.lineEdit_3.setText(str(produto[0][3]))
+    editar_produto.lineEdit_4.setText(str(produto[0][4]))
+    editar_produto.lineEdit_5.setText(str(produto[0][5]))
+    editar_produto.lineEdit_6.setText(str(produto[0][6]))
+def salvar_editar():
+    print("teste")
+
+    
 
 app=QtWidgets.QApplication([])
 formulario_cadastro = uic.loadUi("formulario_cadastro.ui")
-login = uic.loadUi("tela_login.ui")
-lista_produtos = uic.loadUi("listar_dados.ui")
-login.pushButton.clicked.connect(tela_login)
 formulario_cadastro.pushButton.clicked.connect(tela_cadastro)
 formulario_cadastro.pushButton_3.clicked.connect(tela_lista)
+formulario_cadastro.pushButton_2.clicked.connect(formulario_voltar)
+
+login = uic.loadUi("tela_login.ui")
+login.pushButton.clicked.connect(tela_login)
+
+lista_produtos = uic.loadUi("listar_dados.ui")
+lista_produtos.pushButton_7.clicked.connect(excluir_dados)
+lista_produtos.pushButton_5.clicked.connect(lista_voltar)
+
+editar_produto = uic.loadUi("editar_produtos.ui")
+lista_produtos.pushButton_9.clicked.connect(tela_editar)
+editar_produto,pushButton.clicked.connect(salvar_editar)
+editar_produto.pushButton_2.clicked.connect(editar_voltar)
+
 login.show()
 app.exec()
 
